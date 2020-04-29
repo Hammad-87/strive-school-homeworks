7ef4bb5db9a3da50af146d2e6f49b5f5a69c2d8b
Ubeyt Demir <190541075@firat.edu.tr>
2020-04-29 18:03:41 +0300

    last changes before debrief

--- week3/day2/strive-p3-yannick/js/index.js
+++ week3/day2/strive-p3-yannick/js/index.js
@@ -2,15 +2,22 @@ window.onload = function () {
   this.indexPage();
 };
 function Navbar() {
-  const menu = ["Home", "About Us", "Tours", "Contact"];
+  const menu = ["Home", "About", "Tours", "Contact"];
   let navbar = document.querySelector(".navbar-nav");
   this.console.log(navbar);
   for (let i = 0; i < menu.length; i++) {
     let item = menu[i];
     let li = this.addElement(navbar, "li", { className: "nav-item" });
-    let link = this.addElement(li, "a", {
-      className: "nav-link",
-      href: "#",
+    let link = this.addElement(li, "button", {
+      className: "nav-link btn orange-btn",
+      onclick: function () {
+        let functionName = item.toLowerCase();
+        if (item !== "Home") {
+          eval(`${functionName}()`);
+        } else {
+          window.location.replace("/");
+        }
+      },
       innerText: item,
     });
   }
@@ -27,19 +34,25 @@ function indexPage() {
   <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 </div> */
 
-  let jumbotron = addElement(root, "div", { className: "jumbotron" });
+  let jumbotron = addElement(root, "div", {
+    className: "jumbotron",
+    style: `background:url(https://source.unsplash.com/1600x900/?beach);`,
+  });
   let title = addElement(jumbotron, "h1", {
     className: "display-4",
-    innerText: "My Tour Operator",
+    innerText: "Home, home, where I wanted to go",
+    style: "color:white;",
   });
   let description = addElement(jumbotron, "p", {
     className: "lead",
-    innerText: "My Simple Exp.",
+    innerText: "As Coldplay say",
   });
   let divider = addElement(jumbotron, "hr", { className: "my-4" });
-  let action = addElement(jumbotron, "button", {
+  let action = addElement(jumbotron, "a", {
     className: "btn btn-dark",
-    innerText: "My Action",
+    innerText: "What ?",
+    href: "https://www.youtube.com/watch?v=CtTCebEZMCQ",
+    target: "blank",
   });
   renderDestinations(root, "Deals Of Week", destinations);
   renderDestinations(root, "Welcome Summer !", summerOffers);
@@ -58,7 +71,7 @@ function renderDestinations(root, titleText, data) {
   let row = addElement(destinationsContainer, "div", { className: "row" });
   for (let destination of data) {
     let col = addElement(row, "div", {
-      className: "col-xl-3 col-md-4 col-md-3 col-sm-6 col-xs-12",
+      className: "col-xl-4 col-md-4 col-md-3 col-sm-6 col-xs-12",
     });
     let card = addElement(col, "div", {
       className: "card",
@@ -77,6 +90,10 @@ function renderDestinations(root, titleText, data) {
     let p = addElement(cardBody, "p", {
       className: "card-text",
       innerText: `€ ${destination.price}`,
+    });
+    let description = addElement(cardBody, "p", {
+      className: "card-text",
+      innerHTML: paragraph.substring(0, 100),
     });
     let action = addElement(cardBody, "a", {
       href: "#",
@@ -119,4 +136,67 @@ function dealOfTheDay() {
   let price = addElement(infoSection, "h3", {
     innerText: `€ ${randomDestination.price}`,
   });
+  addElement(infoSection, "div", {
+    innerHTML: paragraph.substring(0, 1200),
+    style: "text-align:justify;",
+  });
+}
+
+function Testimonials() {
+  /*
+  fetch(`https://randomuser.me/api/?results=5`)
+    .then((response) => response.json())
+    .then(function (data) {
+      let users = data.results;
+    });
+ */
+  let users = Array.from({ length: 5 });
+  let root = document.querySelector(".root");
+  let;
+}
+
+function about() {
+  let root = document.querySelector(".root");
+  root.innerHTML = "";
+  let contentContainer = addElement(root, "div", {
+    className: "contentContainer",
+    style: "padding:50px 0px;",
+  });
+  let title = addElement(contentContainer, "h2", { innerText: " About Us " });
+  addElement(root, "div", { innerHTML: paragraph });
+}
+
+function tours() {
+  let root = document.querySelector(".root");
+  root.innerHTML = "";
+  let contentContainer = addElement(root, "div", {
+    className: "contentContainer",
+    style: "padding:50px 0px;",
+  });
+  renderDestinations(root, "Tours", [...summerOffers, ...destinations]);
+}
+
+function contact() {
+  let root = document.querySelector(".root");
+  root.innerHTML = "";
+  let contentContainer = addElement(root, "div", {
+    className: "contentContainer",
+    style: "padding:50px 0px;",
+  });
+  let title = addElement(contentContainer, "h2", { innerText: " Contact " });
+  let formHTML = `<form>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="johndoe@strive.school">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Message</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+ <p style="text-align:right;">
+  <button style="min-width:80px;" class="btn btn-dark">Send</button></p>
+</form>`;
+
+  let form = addElement(root, "div", { innerHTML: formHTML });
 }
